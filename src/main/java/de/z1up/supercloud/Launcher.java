c1420946da01e5633f7ad0e9486d68f49ba765ed
chris <christoph.langer100@gmail.com>
2021-02-02 21:36:22 +0100

    implement mongo db
    finally started to work with database, using mongo db

--- src/main/java/de/z1up/supercloud/Launcher.java
+++ src/main/java/de/z1up/supercloud/Launcher.java
@@ -1,11 +1,21 @@
 package de.z1up.supercloud;
 
 import de.z1up.supercloud.core.Core;
+import de.z1up.supercloud.core.mongo.MongoManager;
+import org.json.JSONObject;
 
 public class Launcher {
 
     public static void main(String[] args) {
-        new Core().startUp();
+
+        MongoManager mongoManager = new MongoManager();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(mongoManager::disconnect));
+
+        mongoManager.connect();
+
+        //new Core().startUp();
+
     }
 
 }
---
+++ src/main/java/de/z1up/supercloud/core/mongo/MongoManager.java
@@ -0,0 +1,50 @@
+package de.z1up.supercloud.core.mongo;
+
+import com.google.gson.Gson;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import de.z1up.supercloud.core.file.CloudFile;
+import de.z1up.supercloud.core.file.CloudFolder;
+
+import java.io.IOException;
+
+public class MongoManager {
+
+    public static final Gson GSON = new Gson();
+
+    private MongoClient client;
+    private MongoDatabase database;
+
+    private void createFiles() {
+
+        CloudFolder dir = new CloudFolder("mongo");
+        CloudFile file = new CloudFile(dir, "access.yml");
+
+    }
+
+    public void connect() {
+
+        createFiles();
+
+        MongoConfig config = null;
+        try {
+            config = MongoConfig.load("mongo//mongo.json");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        this.client = MongoClients.create(new MongoClientURI(config.getURI()).toString());
+        this.database = this.client.getDatabase(config.getDatabase());
+        this.database.createCollection("test-collection");
+
+    }
+
+    public void disconnect() {
+        if(this.client != null) {
+            this.client.close();
+        }
+    }
+
+}
